81bc44733453625becb8ad6ebb58fa71737c4a42
srwareham <srwareham@hotmail.com>
2013-11-15 11:24:59 -0500

    Skeleton Pixel.cpp

---
+++ Pixel.cpp
@@ -0,0 +1,62 @@
+//
+//  Pixel.cpp
+//  imagedit
+//
+//  Created by Sean Wareham on 11/15/13.
+//  Copyright (c) 2013 Sean Wareham. All rights reserved.
+//
+
+#include "Pixel.h"
+
+Pixel::Pixel(float r, float g, float b){
+    
+}
+
+void Pixel::setCartCords(int x, int y){
+    
+}
+void Pixel::setX(){
+    
+}
+
+void Pixel::setY(){
+    
+}
+
+int Pixel::getX(){
+    //do math on i,j to find these
+    return 0;
+}
+
+int Pixel::getY(){
+    return 0;
+}
+
+void Pixel::setI(){
+    
+}
+
+void Pixel::setJ(){
+    
+}
+
+int Pixel::getI(){
+    return i;
+}
+
+int getJ(){
+    return j;
+}
+
+//TODO: decide where i want this memory to be allocated. Perhaps on the heap at read time?
+//also need to perhaps have methods return an image instead of modify an existing one
+//this would be much easier for the case of shrinking an image
+float* R(){
+    return r;
+}
+float* G(){
+    return g;
+}
+float* B(){
+    return b;
+}
